0605bc2fa9078ac159a26e8393244fd47d29bf46
DevJayMx <eliasgarcia971@gmail.com>
2023-07-13 15:58:05 -0600

    env variables setted

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,13 +1,14 @@
 import { DataSource } from 'typeorm'
 import { Employees } from './entities/Employees'
+import {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE} from './config'
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "password",
-    database: "keyence",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
     entities: [Employees],
     logging: true,
     synchronize: true
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,14 @@
 import "reflect-metadata";
 import app from "./app";
 import { AppDataSource } from "./db";
+import {PORT} from  './config'
 
 async function main() {
-    const port = 4000;
     try {
         await AppDataSource.initialize()
         console.log("Connected to BD!")
-        app.listen(port)
-        console.log(`Server is on port ${port}`)
+        app.listen(PORT)
+        console.log(`Server is on PORT ${PORT}`)
     } catch (error) {
         console.error(error)
     }
