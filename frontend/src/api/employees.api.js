724bf929cbf6f19b431ea2f996b674ddd159316c
DevJayMx <eliasgarcia971@gmail.com>
2023-07-13 16:22:14 -0600

    Changed url to railway backend

--- frontend/src/api/employees.api.js
+++ frontend/src/api/employees.api.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
 export const createEmployeeReq = async (employee) => {
-    return await axios.post('http://localhost:4000/employee', employee)
+    return await axios.post('https://keyence-production.up.railway.app/employee', employee)
 }
 
 export const getEmployeesReq = async () => {
-    return await axios.get('http://localhost:4000/employee')
+    return await axios.get('https://keyence-production.up.railway.app/employee')
 }
 
 export const getEmployeeReq = async (employeeID) => {
-    return await axios.get(`http://localhost:4000/employee/${employeeID}`)
+    return await axios.get(`https://keyence-production.up.railway.app/employee/${employeeID}`)
 }
 
 export const updateEmployeeReq = async (employeeID, payload) => {
-    return await axios.put(`http://localhost:4000/employee/${employeeID}`, payload)
+    return await axios.put(`https://keyence-production.up.railway.app/employee/${employeeID}`, payload)
 }
 
 export const deleteEmployeeReq = async(employeeID) => {
-    return await axios.delete(`http://localhost:4000/employee/${employeeID}`)
+    return await axios.delete(`https://keyence-production.up.railway.app/employee/${employeeID}`)
 }
