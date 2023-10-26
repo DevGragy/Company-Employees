41e53062efd9ba37707ff2e65f6a5c995c34a945
DevGragy <eliasgarcia971@gmail.com>
2023-10-26 13:15:11 -0600

    url to test endpoint setted

--- frontend/src/api/employees.api.js
+++ frontend/src/api/employees.api.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
 
 export const createEmployeeReq = async (employee) => {
-    return await axios.post('https://keyence-production.up.railway.app/employee', employee)
+    return await axios.post('http://localhost:4000/employee', employee)
 }
 
 export const getEmployeesReq = async () => {
-    return await axios.get('https://keyence-production.up.railway.app/employee')
+    return await axios.get('http://localhost:4000/employee')
 }
 
 export const getEmployeeReq = async (employeeID) => {
-    return await axios.get(`https://keyence-production.up.railway.app/employee/${employeeID}`)
+    return await axios.get(`http://localhost:4000/employee/${employeeID}`)
 }
 
 export const updateEmployeeReq = async (employeeID, payload) => {
-    return await axios.put(`https://keyence-production.up.railway.app/employee/${employeeID}`, payload)
+    return await axios.put(`http://localhost:4000/employee/${employeeID}`, payload)
 }
 
 export const deleteEmployeeReq = async(employeeID) => {
-    return await axios.delete(`https://keyence-production.up.railway.app/employee/${employeeID}`)
+    return await axios.delete(`http://localhost:4000/employee/${employeeID}`)
 }
 
 //PROD: https://keyence-production.up.railway.app/employee
