ce875938e96df6a7d27465d446a85da5b5bca57e
DevGragy <eliasgarcia971@gmail.com>
2023-10-26 13:16:11 -0600

    postgres setted on testing

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,6 +5,6 @@ config()
 export const PORT = process.env.PORT || 4000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'postgres'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'password'
-export const DB_DATABASE = process.env.DB_DATABASE || 'keyence'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'pass'
+export const DB_DATABASE = process.env.DB_DATABASE || 'employees'
 export const DB_PORT = Number(process.env.DB_PORT);
